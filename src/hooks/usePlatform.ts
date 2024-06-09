d11a31f532ccf2ce22bbaf3fba665356cd521ad9
Joni Haryono <jharyono@gmail.com>
2024-06-09 21:31:13 +0700

    Filter games by platform

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,10 +1,11 @@
 import useData from "./useData"
+import { Platform } from "./useGames"
 
-export interface Platform {
-  id: number
-  name: string
-  slug: string
-}
+// export interface Platform {
+//   id: number
+//   name: string
+//   slug: string
+// }
 
 const usePlatform = () => useData<Platform>("/platforms/lists/parents")
 
