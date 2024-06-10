8d83119fd8ffc14807e3c2f9b9c333dd7b783f87
Joni Haryono <jharyono@gmail.com>
2024-06-10 12:52:26 +0700

    fine tune

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -14,7 +14,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platform"}
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
