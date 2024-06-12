efadd5c7afe9049b0f2181d407e3617c1740d92a
Joni Haryono <jharyono@gmail.com>
2024-06-12 19:58:13 +0700

    Ship genre with the app

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,12 +1,5 @@
-import useData from "./useData"
-import { Platform } from "./useGames"
+import platforms from "../data/platforms"
 
-// export interface Platform {
-//   id: number
-//   name: string
-//   slug: string
-// }
-
-const usePlatform = () => useData<Platform>("/platforms/lists/parents")
+const usePlatform = () => ({ data: platforms, error: null })
 
 export default usePlatform
